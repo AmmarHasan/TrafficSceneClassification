4be401bb2c72b4d7ac44bc2cced5048b3929bde0
Ammar Hasan <hasanammar94@gmail.com>
2019-06-29 14:34:42 +0200

    Initial commit

---
+++ Assets/Scripts/Classes/Tuple.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Classes
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 First { get; private set; }
+        public T2 Second { get; private set; }
+        internal Tuple(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+}
