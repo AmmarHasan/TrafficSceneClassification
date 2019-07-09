e417ffdbda32b0f41256c9532f9f17504a0ef4b7
Ammar Hasan <hasanammar94@gmail.com>
2019-07-09 18:44:47 +0200

    Label images

--- minify-images.py
+++ minify-images.py
@@ -1,12 +1,37 @@
 from pathlib import Path
 from PIL import Image
+import numpy as np
+import _pickle as cPickle
 
+noOfLanes = 4
+labels = []
+labelsFileName = 'labels.data'
 counter = 0
-for filePath in Path('Data').glob('**/**/*.jpg'):
+fullResImageDiretory = 'Data'
+lowResImageDirectory = 'TransformedData'
+for filePath in Path(fullResImageDiretory).glob('**/**/*.jpg'):
     im = Image.open(filePath)
     nx,ny = im.size
     # Resize images from 1280x720 to 256x144
-    im.resize((int(nx/5),int(ny/5))).save('TransformedData/'+counter.__str__()+'.jpg')
+    im.resize((int(nx/5),int(ny/5))).save(lowResImageDirectory + '/' + counter.__str__()+'.jpg')
     counter+=1
+    label = int(filePath.parts[1][-1:])
     print(counter)
+    print("label:" + str(label))
+    labels.append(label)
 
+# Generating one hot vector
+npLabels = np.array(labels)
+npLabelsHotVector = np.zeros((npLabels.size, noOfLanes))
+npLabelsHotVector[np.arange(npLabels.size), npLabels-1] = 1
+
+
+with open(lowResImageDirectory + '/' + labelsFileName, 'wb') as filehandle:
+    # store the data as binary data stream
+    cPickle.dump(npLabelsHotVector, filehandle)
+with open(lowResImageDirectory + '/' + labelsFileName, 'rb') as filehandle:
+    # read the data as binary data stream
+    placesList = cPickle.load(filehandle)
+
+
+print(np.array(placesList).argmax(axis=0))
