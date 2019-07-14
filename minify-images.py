e9d9b090817126d26282dd22ff4ba38500eaf9d7
Ammar Hasan <hasanammar94@gmail.com>
2019-07-14 16:01:01 +0200

    Implement store imge as array logic in minif-images.py

--- minify-images.py
+++ minify-images.py
@@ -3,17 +3,30 @@ from PIL import Image
 import numpy as np
 import _pickle as cPickle
 
-noOfLanes = 4
-labels = []
+def load_image(img) :
+    # convert the image to grayscale
+    img = img.convert(mode='L')
+    data = np.asarray(img, dtype="float32")
+    return data
+
 labelsFileName = 'labels.data'
-counter = 0
 fullResImageDirectory = 'Data'
 lowResImageDirectory = 'ImageCapture'
+imagesAsArrayFile = 'imagesAsArray.data'
+noOfLanes = 4
+
+counter = 0
+labels = []
+imgAsArray = []
+
 for filePath in Path(fullResImageDirectory).glob('**/**/*.jpg'):
     im = Image.open(filePath)
     nx,ny = im.size
     # Resize images from 1280x720 to 256x144
     im.resize((int(nx/5),int(ny/5))).save(lowResImageDirectory + '/' + counter.__str__()+'.jpg')
+    imgAsArray.append(load_image(im))
+    im.save(lowResImageDirectory + '/' + counter.__str__()+'.jpg')
+
     counter+=1
     label = int(filePath.parts[1][-1:])
     print(counter)
@@ -26,8 +39,10 @@ npLabelsHotVector = np.zeros((npLabels.size, noOfLanes))
 npLabelsHotVector[np.arange(npLabels.size), npLabels-1] = 1
 
 
-with open(lowResImageDirectory + '/' + labelsFileName, 'wb') as filehandle:
-    # store the data as binary data stream
+# Store Labels  as binary data stream
+with open(labelsFileName, 'wb') as filehandle:
     cPickle.dump(npLabelsHotVector, filehandle)
 
-print(npLabelsHotVector.argmax(axis=0))
+# Store Image Array as binary data stream
+with open(imagesAsArrayFile, 'wb') as filehandle:
+    cPickle.dump(imgAsArray, filehandle)
