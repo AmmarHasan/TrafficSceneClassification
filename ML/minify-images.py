c54e62b82bdc463ce076ffa0a33f85bfb2dec567
Ammar Hasan <hasanammar94@gmail.com>
2019-09-23 02:10:32 +0200

    'minify-images' file only minifies images

--- ML/minify-images.py
+++ ML/minify-images.py
@@ -1,49 +1,32 @@
 from pathlib import Path
 from PIL import Image
-import numpy as np
-import _pickle as cPickle
+import os
 
-def load_image(img) :
-    # convert the image to grayscale
-    img = img.convert(mode='L')
-    data = np.asarray(img, dtype="float32")
-    return data
+def createDirectory(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  return directory
 
-fullResImageDirectory = 'Data'
-lowResImageDirectory = 'ImageCapture'
-labelsFileName = 'labelsAll.data'
-imagesAsArrayFile = 'imagesAsArray.npy'
-noOfLanes = 4
-
-counter = 0
-labels = []
-imgAsArray = []
-
-for filePath in Path(fullResImageDirectory).glob('**/**/*.jpg'):
-    im = Image.open(filePath)
+def resizeSaveImage(src_path, dest_path, image_name) :
+    im = Image.open(src_path)
     nx,ny = im.size
     # Resize images from 1280x720 to 256x144
-    im = im.resize((int(nx/5),int(ny/5)))
-    im.save(lowResImageDirectory + '/' + counter.__str__()+'.jpg')
-    imgAsArray.append(load_image(im))
+    # im = im.resize((int(nx/5),int(ny/5)))
+    im = im.resize((224,128))
+    im.save(dest_path + '/' + image_name +'.jpg')
+
+full_res_image_path = 'Data/ScreenCapture1'
+low_res_segmented_image_path = createDirectory('Data/images_segmented-1/')
+low_res_image_path = createDirectory('Data/images_train-1/')
+
+counter = 0
+for file_path in Path(full_res_image_path).glob('**/**/*_img.jpg'):
+    label = file_path.parts[2][-1:]
+    image_name = str(counter) + "-" + label
+    seg_image_path = str(file_path.parent) + "/" + file_path.name[:-7] + "layer.jpg"
+
+    resizeSaveImage(file_path, low_res_image_path, image_name)
+    resizeSaveImage(seg_image_path, low_res_segmented_image_path, image_name)
 
     counter+=1
-    label = int(filePath.parts[1][-1:])
     print(counter)
-    print("label:" + str(label))
-    labels.append(label)
-
-# Generating one hot vector
-npLabels = np.array(labels)
-npLabelsHotVector = np.zeros((npLabels.size, noOfLanes))
-npLabelsHotVector[np.arange(npLabels.size), npLabels-1] = 1
-
-print(npLabels.shape)
-np.save(labelsFileName, npLabels)
-npLabels = None
-print(npLabelsHotVector.shape)
-np.save("labelsVector.npy", npLabelsHotVector)
-
-
-print("writing image array")
-np.save(imagesAsArrayFile, imgAsArray)
