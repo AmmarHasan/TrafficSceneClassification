2dfa35544a62558637da1708d2a11dfe3abb9193
Ammar Hasan <hasanammar94@gmail.com>
2019-09-26 17:08:35 +0200

    [ignore] better multi input keras model

--- ML/multi-input.py
+++ ML/multi-input.py
@@ -21,9 +21,42 @@ numberOfLanes = 4
 imgWidth = 224
 imgHeight = 128
 lrs = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005]
-EPOCHS = 10
+EPOCHS = 20
 BATCH_SIZE = 64
 
+# second input model
+visible2 = Input(shape=(2))
+
+# first input model
+visible1 = Input(shape=(imgHeight, imgWidth, 3))
+conv11 = keras.layers.Conv2D(32, kernel_size=4, activation='relu')(visible1)
+pool11 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv11)
+conv12 = keras.layers.Conv2D(16, kernel_size=4, activation='relu')(pool11)
+pool12 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv12)
+flat1 = Flatten()(pool12)
+
+
+flat2 = Flatten()(visible2)
+
+# merge input models
+merge = keras.layers.concatenate([flat1, flat2])
+
+# interpretation model
+hidden1 = Dense(10, activation='relu')(merge)
+hidden2 = Dense(10, activation='relu')(hidden1)
+output = Dense(10, activation='sigmoid')(hidden2)
+model = Model(inputs=[visible1, visible2], outputs=output)
+
+# summarize layers
+model.summary()
+sgd = keras.optimizers.SGD(lr=lrs[2])
+model.compile(optimizer=sgd,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+imagesAsArray = []
+point_coordinates = []
+labelsHotVectors = []
 for filePath in Path(trainDirectory).glob('*.npy'):
   label = str(int(filePath.parts[-1][:-4]))
   imageAsArray = np.load(filePath, allow_pickle=True)
@@ -32,49 +65,16 @@ for filePath in Path(trainDirectory).glob('*.npy'):
   imageAsArray = imageAsArray.reshape(imgHeight, imgWidth, 3)
 
   points_data = np.load(pointsDataDirectory+label+'.npy', allow_pickle=True)
-  # points_training_data = np.load(pointsTrainingDirectory+label+'.npy', allow_pickle=True) # points_label_data = np.load(pointsLabelDirectory+label+'.npy', allow_pickle=True) # for point_data,point_label in zip(points_training_data, points_label_data): # pixel_data = point_data[0]
   point_coordinate = np.asarray([point[0] for point in points_data])
-  # point_coordinate = point_coordinate.reshape(-1, 2)
   point_label = np.asarray([point[1] for point in points_data])
-  print(point_coordinate.shape)
-  print(point_label.shape)
-  # for point_coordinate,point_label in points_data: #   print(imageAsArray.shape) #   print(point_coordinate.shape) #   print(point_label)
-    # first input model
-  visible1 = Input(shape=(imgHeight, imgWidth, 3))
-  conv11 = keras.layers.Conv2D(32, kernel_size=4, activation='relu')(visible1)
-  pool11 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv11)
-  conv12 = keras.layers.Conv2D(16, kernel_size=4, activation='relu')(pool11)
-  pool12 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv12)
-  flat1 = Flatten()(pool12)
-  # second input model
-  visible2 = Input(shape=(2))
-  # conv21 = keras.layers.Conv2D(32, kernel_size=4, activation='relu')(visible2)
-  # pool21 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv21)
-  # conv22 = keras.layers.Conv2D(16, kernel_size=4, activation='relu')(pool21)
-  # pool22 = keras.layers.MaxPooling2D(pool_size=(2, 2))(conv22)
-  # flat2 = Flatten()(pool22)
-  flat2 = Flatten()(visible2)
-  # merge input models
-  merge = keras.layers.concatenate([flat1, flat2])
-  # merge = keras.layers.concatenate([flat1, visible2])
-  # interpretation model
-  hidden1 = Dense(10, activation='relu')(merge)
-  hidden2 = Dense(10, activation='relu')(hidden1)
-  output = Dense(10, activation='sigmoid')(hidden2)
-  model = Model(inputs=[visible1, visible2], outputs=output)
-  # summarize layers
-  print(model.summary())
-  sgd = keras.optimizers.SGD(lr=lrs[2])
-  model.compile(optimizer=sgd,
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
-  imagesAsArray = [imageAsArray for _ in point_coordinate]
   npLabels = np.array([segmented_dict.index(label) for label in point_label])
   npLabelsHotVector = to_categorical(npLabels)
-  print(npLabelsHotVector)
-  # npLabelsHotVector[np.arange(npLabels.size), "sky"] = 1
-  model.fit([imagesAsArray,point_coordinate], npLabelsHotVector, epochs=EPOCHS, batch_size=BATCH_SIZE)
-  break
-  break
-    
+  imagesAsArray.append(imageAsArray)
+  point_coordinates.append(point_coordinate)
+  labelsHotVectors.append(npLabelsHotVector)
 
+for i in range(0, len(imagesAsArray)):
+  imagesAsArrayDuplicate = [imageAsArray for _ in point_coordinates[i]]
+  print(np.asarray(imagesAsArrayDuplicate).shape,np.asarray(point_coordinates[i]).shape, np.asarray(labelsHotVectors[i]).shape)
+  model.fit([imagesAsArrayDuplicate,point_coordinates[i]], labelsHotVectors[i], epochs=EPOCHS)
+  # model.fit([imagesAsArrayDuplicate,point_coordinates[i]], labelsHotVectors[i], epochs=EPOCHS, batch_size=BATCH_SIZE)
